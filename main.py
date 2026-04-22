ef2dafba9caec8cf9578096785740adf68eb700a
Marcio Ferreira <Marcio.Ferreira@fit-tecnologia.org.br>
2026-04-22 11:17:03 -0400

    ok

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
